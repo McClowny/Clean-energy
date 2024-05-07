a326ca8f507e5cfbbc98d758c70f17fed85fb081
Gage B <165205093+BulldogM@users.noreply.github.com>
2024-05-07 15:05:09 -0500

    Java Lava Bava Nava

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const testSelect = document.getElementById("testselect");
 const transportSelector = document.getElementById("select-transport");
+const householdEnergy = [
+// Fossil Fuels //
+    {
+        id:document.getElementById("energy1"),
+        value:document.getElementById("energy1").value
+    }
+// Solar Panels //
+    {
+        id:document.getElementById("energy2"),
+        value:document.getElementById("energy2").value
+    }
+// Water Wheel //
+    {
+        id:document.getElementById("energy3"),
+        value:document.getElementById("energy3").value
+    }
+// Power Grid //
+    {
+        id:document.getElementById("energy4"),
+        value:document.getElementById("energy4").value
+    }
+]
 
 textSelect.innerText = transportSelector.value;
-console.log(textSelect.innerText);
