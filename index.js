c910d56b05a218e6777aabf85403439b727bcf0b
Gage B <165205093+BulldogM@users.noreply.github.com>
2024-05-07 15:05:45 -0500

    yes

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const householdEnergy = [
     {
         id:document.getElementById("energy1"),
         value:document.getElementById("energy1").value
-    }
+    },
 // Solar Panels //
     {
         id:document.getElementById("energy2"),
         value:document.getElementById("energy2").value
-    }
+    },
 // Water Wheel //
     {
         id:document.getElementById("energy3"),
         value:document.getElementById("energy3").value
-    }
+    },
 // Power Grid //
     {
         id:document.getElementById("energy4"),
