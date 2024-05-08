993239ff5f611ffbf4b9351d959015a00b7362de
Gage B <165205093+BulldogM@users.noreply.github.com>
2024-05-08 08:32:56 -0500

    yes

--- index.js
+++ index.js
@@ -6,27 +6,32 @@ const householdEnergy = [
 // Fossil Fuels //
     {
         id:document.getElementById("energy1"),
-        value:document.getElementById("energy1").value
+        value:document.getElementById("energy1").value,
+        isChecked:document.getElementById("energy1").checked
     },
 // Solar Panels //
     {
         id:document.getElementById("energy2"),
-        value:document.getElementById("energy2").value
+        value:document.getElementById("energy2").value,
+        isChecked:document.getElementById("energy2").checked
     },
 // Water Wheel //
     {
         id:document.getElementById("energy3"),
-        value:document.getElementById("energy3").value
+        value:document.getElementById("energy3").value,
+        isChecked:document.getElementById("energy3").checked
     },
 // Wind Turbine //
     {
         id:document.getElementById("energy4"),
-        value:document.getElementById("energy4").value
+        value:document.getElementById("energy4").value,
+        isChecked:document.getElementById("energy4").checked
     },
 // Power Grid //
     {
         id:document.getElementById("energy5"),
-        value:document.getElementById("energy5").value
+        value:document.getElementById("energy5").value,
+        isChecked:document.getElementById("energy5").checked
     }
 ]
 
