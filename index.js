ce5243b159829914911f232ecbe7c3c86a147ea6
Gage B <165205093+BulldogM@users.noreply.github.com>
2024-05-09 13:12:08 -0500

    yeee

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
 const testSelect = document.getElementById("testselect");
-const redBar = document.querySelector(".red .percentage-bar");
-const greenBar = document.querySelector(".green .percentage-bar");
-const blueBar = document.querySelector(".blue .percentage-bar");
+const redBar = document.querySelector(".emissions .percentage-bar");
+const greenBar = document.querySelector(".costs .percentage-bar");
+const blueBar = document.querySelector(".time .percentage-bar");
 
 let transportSelect = document.getElementById("select-transport");
 
 const householdEnergy = [
-    // Fossil Fuels //
+    // Turn off unecessary lights //
     {
         id:document.getElementById("energy1"),
         value:document.getElementById("energy1").value,
         isChecked:document.getElementById("energy1").checked
     },
-    // Solar Panels //
+    // Hook up solar panels //
     {
         id:document.getElementById("energy2"),
         value:document.getElementById("energy2").value,
         isChecked:document.getElementById("energy2").checked
     },
-    // Water Wheel //
+    // Change your transportation //
     {
         id:document.getElementById("energy3"),
         value:document.getElementById("energy3").value,
         isChecked:document.getElementById("energy3").checked
-    },
-    // Wind Turbine //
-    {
-        id:document.getElementById("energy4"),
-        value:document.getElementById("energy4").value,
-        isChecked:document.getElementById("energy4").checked
-    },
-    // Power Grid //
-    {
-        id:document.getElementById("energy5"),
-        value:document.getElementById("energy5").value,
-        isChecked:document.getElementById("energy5").checked
     }
 ]
 const Selector = {
     Foot:[20,24,12],
     Bike:[14,63,28],
     Car:[74,28,30],
+    "Electric Car":[14,18,73],
+    Bus:[53,28,72],
+    Train:[43,91,3]
 }
 
 transportSelect.onclick = setValue;
