974db832e3e270c80bbb2db20af97c2de17c4baa
Gage B <165205093+BulldogM@users.noreply.github.com>
2024-05-10 12:22:40 -0500

    yee

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const testSelect = document.getElementById("testselect");
 const emissionBar = document.querySelector(".emissions .percentage-bar");
 const costBar = document.querySelector(".costs .percentage-bar");
 const timeBar = document.querySelector(".time .percentage-bar");
-const emissionPostBar = document.querySelector(".emissions .percentage-bar div");
-const costPostBar = document.querySelector(".costs .percentage-bar div");
-const timePostBar = document.querySelector(".time .percentage-bar div");
+const emissionPostBar = document.querySelector(".emissions .post-percentage-bar");
+const costPostBar = document.querySelector(".costs .post-percentage-bar");
+const timePostBar = document.querySelector(".time .post-percentage-bar");
 const transportSelect = document.getElementById("select-transport");
 const changeTransportSelect = document.getElementById("change-transport");
 
@@ -28,7 +28,7 @@ const householdEnergy = [
         value: [0,0,0]
     }
 ]
-const Selector = {
+const selector = {
     Foot:[0,0,80],
     Bike:[0,5,65],
     Car:[60,65,25],
@@ -37,23 +37,26 @@ const Selector = {
     Train:[10,20,40]
 }
 
+
+
 transportSelect.onclick = setValue;
 
 function setValue() {
-    const valuePool = Selector[transportSelect.value];
-    const valuePoolPost = Selector[changeTransportSelect.value];
+    const valuePool = selector[transportSelect.value];
+    const postValuePool = selector[changeTransportSelect.value];
     emissionBar.style.width = valuePool[0] + 20 + "%";
     costBar.style.width = valuePool[1] + 20 + "%";
     timeBar.style.width = valuePool[2] + 20 + "%";
-    emissionPostBar.style.width = valuePoolPost[0] + 20 + "%";
-    costPostBar.style.width = valuePoolPost[1] + 20 + "%";
-    timePostBar.style.width = valuePoolPost[2] + 20 + "%";
+    emissionPostBar.style.width = postValuePool[0] + 20 + "%";
+    costPostBar.style.width = postValuePoolt[1] + 20 + "%";
+    timePostBar.style.width = postValuePool[2] + 20 + "%";
 }
 
 changeTransportSelect.onclick = checkSelector;
 
 function checkSelector() {
-    if (Selector[transportSelect.value] = Selector[changeTransportSelect]) {
+    const int = transportSelect.value.localeCompare(changeTransportSelect.value);
+    if (int) {
     document.querySelector("#foreground").style.backgroundColor = "blue";
     } else {
     document.querySelector("#foreground").style.backgroundColor = "red";
