d0b629dd1acdb60dd9d54ba0729fdbde745b9639
Gage B <165205093+BulldogM@users.noreply.github.com>
2024-05-14 12:20:59 -0500

    yee

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const testSelect = document.getElementById("testselect");
-const emissionBar = document.querySelector(".emissions .percentage-bar");
-const costBar = document.querySelector(".costs .percentage-bar");
-const timeBar = document.querySelector(".time .percentage-bar");
-const emissionPostBar = document.querySelector(".emissions .post-percentage-bar");
-const costPostBar = document.querySelector(".costs .post-percentage-bar");
-const timePostBar = document.querySelector(".time .post-percentage-bar");
+const emissionBar = document.querySelector(".emissions .ruler-bar .percentage-bar");
+const costBar = document.querySelector(".costs .ruler-bar .percentage-bar");
+const timeBar = document.querySelector(".time .ruler-bar .percentage-bar");
+const emissionPostBar = document.querySelector(".emissions .ruler-bar .post-percentage-bar");
+const costPostBar = document.querySelector(".costs .ruler-bar .post-percentage-bar");
+const timePostBar = document.querySelector(".time .ruler-bar .post-percentage-bar");
 const transportSelect = document.getElementById("select-transport");
 const changeTransportSelect = document.getElementById("change-transport");
 const warning = document.querySelector("#warning");
@@ -41,14 +41,14 @@ let isCheckboxSelectorEnabled = 0;
 
 calculateButton.onclick = checkSelector;
 
-function setValue() {
-    const valuePool = selector[transportSelect.value];
-    const postValuePool = selector[changeTransportSelect.value];
+function setValue(elmnt1, elmnt2) {
+    const valuePool = elmnt1;
+    const postValuePool = elmnt2;
     emissionBar.style.width = valuePool[0] + 20 + "%";
     costBar.style.width = valuePool[1] + 20 + "%";
     timeBar.style.width = valuePool[2] + 20 + "%";
     emissionPostBar.style.width = postValuePool[0] + 20 + "%";
-    costPostBar.style.width = postValuePoolt[1] + 20 + "%";
+    costPostBar.style.width = postValuePool[1] + 20 + "%";
     timePostBar.style.width = postValuePool[2] + 20 + "%";
 }
 
@@ -61,13 +61,13 @@ function checkSelector() {
             warningPhrase.innerText = "You picked the same for both dropdowns. You don't need to enable this option!";
         } else if (selector[elmnt1][0]>=selector[elmnt2][0]) {
             warning.style.opacity = "0%";
-            setValue;
+            setValue(selector[elmnt1],selector[elmnt2]);
         } else {
             warning.style.opacity = "100%";
             warningPhrase.innerText = "You picked an option worse for the environment. Pick something else!";
         }
     } else {
         warning.style.opacity = "0%";
-        setValue;
+        setValue(selector[elmnt1],selector[elmnt2]);
     }
 }
